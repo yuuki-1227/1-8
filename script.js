b156379cde6bd19c9ebd53d7e3d0e745236c429e
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-22 18:52:36 +0900

    [Update]クリックアクション

--- script.js
+++ script.js
@@ -28,3 +28,37 @@ $(function(){
   $('.picture-list:nth-child(5)').hide();
   $('.picture-list:nth-child(5)').show();
 });
+
+$(function(){
+  $('.picture-list:nth-child(6)').mouseover(function(){
+    $('.picture-list:nth-child(6)').css({'background-color':'#0000ff'});
+  });
+  $('.picture-list:nth-child(6)').mouseout(function(){
+    $('.picture-list:nth-child(6)').css({'background-color':'yellow'});
+  });
+});
+
+$(function(){
+  $('.picture-list:nth-child(7)').mouseover(function(){
+    $('.picture-list:nth-child(7)').addClass('picture-list-ex');
+  });
+  $('.picture-list:nth-child(7)').mouseout(function(){
+    $('.picture-list:nth-child(7)').removeClass('picture-list-ex');
+  });
+});
+
+$(function(){
+  $('.picture-list:nth-child(8)').on('click',function(){
+    $('.picture-list:nth-child(8)').addClass('picture-list-ex');
+  });
+
+  $('.picture-list:nth-child(8)').mouseout(function(){
+    $('.picture-list:nth-child(8)').removeClass('picture-list-ex');
+  });
+});
+
+$(function(){
+  $('.picture-list').on('click',function(){
+    $(this).slideUp();
+  });
+});
